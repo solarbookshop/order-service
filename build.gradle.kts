75b78b4fffce4ede1a5aae035e4f27a45b1e413d
kartikdhiman <59189590+kartikdhiman@users.noreply.github.com>
2025-11-29 13:47:14 +0530

    feat: configure Spring Cloud with connection settings and retry mechanism

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,11 +24,15 @@ repositories {
     mavenCentral()
 }
 
+extra["springCloudVersion"] = "2025.1.0"
+
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-flyway")
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
     implementation("org.flywaydb:flyway-database-postgresql")
     runtimeOnly("org.postgresql:postgresql")
     runtimeOnly("org.postgresql:r2dbc-postgresql")
@@ -43,6 +47,12 @@ dependencies {
     testImplementation("org.testcontainers:testcontainers-r2dbc")
     testImplementation("com.squareup.okhttp3:mockwebserver:5.3.2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+  imports {
+    mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+  }
 }
 
 tasks.withType<Test> {
