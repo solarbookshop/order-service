3d6e7945f1d23ecfe9b7e8bdaee30c06d0af5c01
Kartik Dhiman <59189590+kartikdhiman@users.noreply.github.com>
2025-12-06 15:23:56 +0530

    feat: implement order dispatch functionality with RabbitMQ integration

--- src/main/java/com/solarbookshop/orderservice/order/domain/OrderService.java
+++ src/main/java/com/solarbookshop/orderservice/order/domain/OrderService.java
@@ -2,6 +2,7 @@ package com.solarbookshop.orderservice.order.domain;
 
 import com.solarbookshop.orderservice.book.Book;
 import com.solarbookshop.orderservice.book.BookClient;
+import com.solarbookshop.orderservice.order.event.OrderDispatchedMessage;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -14,6 +15,10 @@ public class OrderService {
   public OrderService(OrderRepository orderRepository, BookClient bookClient) {
     this.orderRepository = orderRepository;
     this.bookClient = bookClient;
+  }
+
+  public static Order buildRejectedOrder(String isbn, int quantity) {
+    return Order.of(isbn, null, null, quantity, OrderStatus.REJECTED);
   }
 
   public Flux<Order> getAllOrders() {
@@ -31,7 +36,23 @@ public class OrderService {
     return Order.of(book.isbn(), book.title() + " - " + book.author(), book.price(), quantity, OrderStatus.ACCEPTED);
   }
 
-  public static Order buildRejectedOrder(String isbn, int quantity) {
-    return Order.of(isbn, null, null, quantity, OrderStatus.REJECTED);
+  public Flux<Order> consumeOrderDispatchedEvent(Flux<OrderDispatchedMessage> flux) {
+    return flux.flatMap(message -> orderRepository.findById(message.orderId()))
+            .map(this::buildDispatchedOrder)
+            .flatMap(orderRepository::save);
+  }
+
+  private Order buildDispatchedOrder(Order existingOrder) {
+    return new Order(
+            existingOrder.id(),
+            existingOrder.bookIsbn(),
+            existingOrder.bookName(),
+            existingOrder.bookPrice(),
+            existingOrder.quantity(),
+            OrderStatus.DISPATCHED,
+            existingOrder.createdDate(),
+            existingOrder.lastModifiedDate(),
+            existingOrder.version()
+    );
   }
 }
---
+++ src/main/java/com/solarbookshop/orderservice/order/event/OrderDispatchedMessage.java
@@ -0,0 +1,4 @@
+package com.solarbookshop.orderservice.order.event;
+
+public record OrderDispatchedMessage(Long orderId) {
+}
