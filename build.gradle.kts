71513a80aaf6765d408e6c84105b29993c9a32c1
Kartik Dhiman <59189590+kartikdhiman@users.noreply.github.com>
2026-06-18 22:22:12 +0530

    chore: update Spring Boot version to 4.1.0 and Spring Cloud version to 2025.1.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    java
-    id("org.springframework.boot") version "4.0.0"
-    id("io.spring.dependency-management") version "1.1.7"
+  java
+  id("org.springframework.boot") version "4.1.0"
+  id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.example"
@@ -9,48 +9,48 @@ version = "0.0.1-SNAPSHOT"
 description = "order-service"
 
 java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(25)
-    }
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(25)
+  }
 }
 
 configurations {
-    compileOnly {
-        extendsFrom(configurations.annotationProcessor.get())
-    }
+  compileOnly {
+    extendsFrom(configurations.annotationProcessor.get())
+  }
 }
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
-extra["springCloudVersion"] = "2025.1.0"
+extra["springCloudVersion"] = "2025.1.2"
 
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-flyway")
-    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("org.springframework.cloud:spring-cloud-stream-binder-rabbit")
-    implementation("org.springframework.cloud:spring-cloud-starter-config")
-    implementation("org.flywaydb:flyway-database-postgresql")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    runtimeOnly("org.postgresql:postgresql")
-    runtimeOnly("org.postgresql:r2dbc-postgresql")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-    testImplementation("org.springframework.boot:spring-boot-starter-flyway-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-data-r2dbc-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
-    testImplementation("org.springframework.boot:spring-boot-starter-webflux-test")
-    testImplementation("org.springframework.boot:spring-boot-testcontainers")
-    testImplementation("org.springframework.cloud:spring-cloud-stream-test-binder")
-    testImplementation("org.testcontainers:testcontainers-junit-jupiter")
-    testImplementation("org.testcontainers:testcontainers-postgresql")
-    testImplementation("org.testcontainers:testcontainers-r2dbc")
-    testImplementation("com.squareup.okhttp3:mockwebserver3:5.3.2")
-    testImplementation("com.squareup.okhttp3:okhttp:5.3.2")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+  implementation("org.springframework.boot:spring-boot-starter-flyway")
+  implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+  implementation("org.springframework.boot:spring-boot-starter-validation")
+  implementation("org.springframework.boot:spring-boot-starter-webflux")
+  implementation("org.springframework.cloud:spring-cloud-stream-binder-rabbit")
+  implementation("org.springframework.cloud:spring-cloud-starter-config")
+  implementation("org.flywaydb:flyway-database-postgresql")
+  developmentOnly("org.springframework.boot:spring-boot-devtools")
+  runtimeOnly("org.postgresql:postgresql")
+  runtimeOnly("org.postgresql:r2dbc-postgresql")
+  annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+  testImplementation("org.springframework.boot:spring-boot-starter-flyway-test")
+  testImplementation("org.springframework.boot:spring-boot-starter-data-r2dbc-test")
+  testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
+  testImplementation("org.springframework.boot:spring-boot-starter-webflux-test")
+  testImplementation("org.springframework.boot:spring-boot-testcontainers")
+  testImplementation("org.springframework.cloud:spring-cloud-stream-test-binder")
+  testImplementation("org.testcontainers:testcontainers-junit-jupiter")
+  testImplementation("org.testcontainers:testcontainers-postgresql")
+  testImplementation("org.testcontainers:testcontainers-r2dbc")
+  testImplementation("com.squareup.okhttp3:mockwebserver3:5.3.2")
+  testImplementation("com.squareup.okhttp3:okhttp:5.3.2")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 dependencyManagement {
@@ -60,11 +60,11 @@ dependencyManagement {
 }
 
 tasks.withType<Test> {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
 
 tasks.bootBuildImage {
-    builder.set("paketobuildpacks/builder-noble-java-tiny:latest")
-    imageName.set(project.name)
-    environment.set(mapOf("BP_JVM_VERSION" to "25"))
+  builder.set("paketobuildpacks/builder-noble-java-tiny:latest")
+  imageName.set(project.name)
+  environment.set(mapOf("BP_JVM_VERSION" to "25"))
 }
